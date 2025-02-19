2a419b17069bed7df95fea7ef383d5713f186f49
Rodrigo souza <124813706+rodrigosouza974@users.noreply.github.com>
2025-02-19 20:19:55 -0300

    Player limiter on x-axis

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed = 20.0f;
+    public float xRange = 8.0f;
+
     private float horizontalInput;
     public bool gameOver = false ;
     public bool isOnGround = true;
@@ -21,8 +23,17 @@ public class PlayerController : MonoBehaviour
     }
     void Update()
     {
-            horizontalInput = Input.GetAxis("Horizontal");
-            transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);  
+        if (transform.position.x <= -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x >= xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);  
     }
 
     private void OnCollisionEnter(Collision collision){
