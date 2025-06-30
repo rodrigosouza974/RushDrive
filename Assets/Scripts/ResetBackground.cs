8c8fefbfde7e1c92dd346954492b2c0c6b928d54
Rodrigo souza <124813706+rodrigosouza974@users.noreply.github.com>
2025-06-29 23:25:15 -0300

    New Background

--- Assets/Scripts/ResetBackground.cs
+++ Assets/Scripts/ResetBackground.cs
@@ -4,21 +4,21 @@ using UnityEngine;
 
 public class ResetBackground : MonoBehaviour
 {
-    private float startPosZ; // Posição inicial do fundo
-    private float endPosZ;   // Posição final do fundo
+    private float startPosZ; // Posiï¿½ï¿½o inicial do fundo
+    private float endPosZ;   // Posiï¿½ï¿½o final do fundo
     private float backgroundSize; // Tamanho total do fundo
 
     void Start()
     {
-        startPosZ = 50f;      // Posição inicial (onde começa o movimento)
-        endPosZ = -6.17f;     // Posição final (onde termina o movimento)
+        startPosZ = 55f;      // Posiï¿½ï¿½o inicial (onde comeï¿½a o movimento)
+        endPosZ = -10f;     // Posiï¿½ï¿½o final (onde termina o movimento)
         backgroundSize = GetComponent<BoxCollider>().size.z; // Pegando o tamanho do fundo
     }
 
     void Update()
     {
 
-        // Se o fundo ultrapassar a posição final (-10.6), reposiciona para 50
+        // Se o fundo ultrapassar a posiï¿½ï¿½o final (-10), reposiciona para 50
         if (transform.position.z <= endPosZ)
         {
             transform.position = new Vector3(transform.position.x, transform.position.y, startPosZ);
